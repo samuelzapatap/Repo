d2d444a38ac0078852772bb4559416672f0d2bb4
samuelzapatap <100789745+samuelzapatap@users.noreply.github.com>
2022-05-15 20:23:32 -0500

    samuel

---
+++ sistema/src/sistema/Aspirantes/Aspirantes.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema.empleadosBL;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Aspirantes {
+    
+  private int id, edad;  
+  
+  private String nombre, nivel_academico, correo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNivel_academico() {
+        return nivel_academico;
+    }
+
+    public void setNivel_academico(String nivel_academico) {
+        this.nivel_academico = nivel_academico;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+  
+    
+    
+}
+ 
